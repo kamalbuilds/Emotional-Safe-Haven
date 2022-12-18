18df876e8d8635de6bcae0a5e6c663cbbb08fa52
Kamal Nayan <geniusamansingh@gmail.com>
2022-12-18 04:27:46 +0000

    typos fixed + doc added

--- src/components/dashboard-sidebar.js
+++ src/components/dashboard-sidebar.js
@@ -18,7 +18,7 @@ const items = [
   {
     href: "/account",
     imageUrl: "/static/images/products/product_3.png",
-    title: "MonaLisa",
+    title: "Aman Singh",
   },
   {
     href: "/settings",
@@ -115,7 +115,7 @@ export const DashboardSidebar = (props) => {
             my: 0,
           }}
         />
-        <CardHeader title="Active Counsellars" />
+        <CardHeader title="Active Counsellors" />
         <Box sx={{ flexGrow: 0 }}>
           {items.map((item) => (
             <NavItem key={item.title} href={item.href} title={item.title} />
