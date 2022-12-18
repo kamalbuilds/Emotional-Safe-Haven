142ffd76f9d8c4ef0bac1d9ff0045b8f45d800d4
Amisha Pandey <pamisha0606@gmail.com>
2022-12-18 12:41:43 +0530

    reduced image size

--- src/__mocks__/products.js
+++ src/__mocks__/products.js
@@ -1,34 +1,34 @@
-import { v4 as uuid } from 'uuid';
+import { v4 as uuid } from "uuid";
 
 export const products = [
   {
     id: uuid(),
-    createdAt: '27/03/2019',
-    media: '/static/images/products/happy2.jpg',
+    createdAt: "27/03/2019",
+    media: "/static/images/products/happy3.jpg",
   },
   {
     id: uuid(),
-    createdAt: '31/03/2019',
-    media: '/static/images/products/happy2.jpg',
+    createdAt: "31/03/2019",
+    media: "/static/images/products/happy3.jpg",
   },
   {
     id: uuid(),
-    createdAt: '03/04/2019',
-    media: '/static/images/products/happy2.jpg',
+    createdAt: "03/04/2019",
+    media: "/static/images/products/happy3.jpg",
   },
   {
     id: uuid(),
-    createdAt: '04/04/2019',
-    media: '/static/images/products/happy2.jpg',
+    createdAt: "04/04/2019",
+    media: "/static/images/products/happy3.jpg",
   },
   {
     id: uuid(),
-    createdAt: '04/04/2019',
-    media: '/static/images/products/happy2.jpg',
+    createdAt: "04/04/2019",
+    media: "/static/images/products/happy3.jpg",
   },
   {
     id: uuid(),
-    createdAt: '04/04/2019',
-    media: '/static/images/products/happy2.jpg',
-  }
+    createdAt: "04/04/2019",
+    media: "/static/images/products/happy3.jpg",
+  },
 ];
