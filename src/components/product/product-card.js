8ed7e645f9dc62ee28b91ad0eacd42794abfee8b
Amisha Pandey <pamisha0606@gmail.com>
2022-12-18 12:11:35 +0530

    Updated peace pocket

--- src/components/product/product-card.js
+++ src/components/product/product-card.js
@@ -1,89 +1,45 @@
 import PropTypes from 'prop-types';
 import { Avatar, Box, Card, CardContent, Divider, Grid, Typography } from '@mui/material';
-import { Clock as ClockIcon } from '../../icons/clock';
-import { Download as DownloadIcon } from '../../icons/download';
+import FavoriteRoundedIcon from "@mui/icons-material/FavoriteRounded";
 
 export const ProductCard = ({ product, ...rest }) => (
   <Card
     sx={{
-      display: 'flex',
-      flexDirection: 'column',
-      height: '100%'
+      display: "flex",
+      flexDirection: "column",
+      height: "100%",
     }}
     {...rest}
   >
     <CardContent>
       <Box
         sx={{
-          display: 'flex',
-          justifyContent: 'center',
-          pb: 3
+          display: "flex",
+          mx: "auto",
+          width: "140px",
+          "& img": {
+            width: "100%",
+          },
         }}
       >
-        <Avatar
-          alt="Product"
-          src={product.media}
-          variant="square"
-        />
+        <img alt="Memory" src={product.media} />
       </Box>
-      <Typography
-        align="center"
-        color="textPrimary"
-        gutterBottom
-        variant="h5"
-      >
-        {product.title}
-      </Typography>
-      <Typography
-        align="center"
-        color="textPrimary"
-        variant="body1"
-      >
-        {product.description}
-      </Typography>
     </CardContent>
     <Box sx={{ flexGrow: 1 }} />
     <Divider />
     <Box sx={{ p: 2 }}>
-      <Grid
-        container
-        spacing={2}
-        sx={{ justifyContent: 'space-between' }}
-      >
+      <Grid container spacing={2} sx={{ justifyContent: "space-between" }}>
         <Grid
           item
           sx={{
-            alignItems: 'center',
-            display: 'flex'
+            alignItems: "center",
+            display: "flex",
+            justifyContent: "center",
           }}
         >
-          <ClockIcon color="action" />
-          <Typography
-            color="textSecondary"
-            display="inline"
-            sx={{ pl: 1 }}
-            variant="body2"
-          >
-            Updated 2hr ago
-          </Typography>
-        </Grid>
-        <Grid
-          item
-          sx={{
-            alignItems: 'center',
-            display: 'flex'
-          }}
-        >
-          <DownloadIcon color="action" />
-          <Typography
-            color="textSecondary"
-            display="inline"
-            sx={{ pl: 1 }}
-            variant="body2"
-          >
-            {product.totalDownloads}
-            {' '}
-            Downloads
+          <FavoriteRoundedIcon color="secondary" />
+          <Typography color="textSecondary" display="inline" sx={{ pl: 1 }} variant="body2">
+            Baby's first birthday
           </Typography>
         </Grid>
       </Grid>
